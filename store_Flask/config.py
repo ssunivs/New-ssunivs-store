80d08f8cb5487fbc4a746de45bfcc14778d8f3fa
clroot <abcdkh1209@gmail.com>
2021-12-29 21:26:25 +0900

    reformat all code

--- store_Flask/config.py
+++ store_Flask/config.py
@@ -1,9 +1,10 @@
-from .ssunivs.dbsetting import db as dbsetting
+from ssunivs.dbsetting import db as dbsetting
+
 db = {
-    'user'     :  dbsetting['user'],
-    'password' :  dbsetting['password'],
-    'host'     :  dbsetting['host'],
-    'port'     :  dbsetting['port'],
-    'database' : dbsetting['database']
+    'user': dbsetting['user'],
+    'password': dbsetting['password'],
+    'host': dbsetting['host'],
+    'port': dbsetting['port'],
+    'database': dbsetting['database']
 }
-SQLALCHEMY_DATABASE_URL = "mysql://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8" 
+SQLALCHEMY_DATABASE_URL = "mysql://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
--- store_Flask/ssunivs/__init__.py
+++ store_Flask/ssunivs/__init__.py
@@ -1,18 +1,21 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+
 import store_Flask.config as config
 
-db=SQLAlchemy()
-migrate=Migrate()
+db = SQLAlchemy()
+migrate = Migrate()
+
+
 def create_app():
-    app=Flask(__name__)
+    app = Flask(__name__)
     app.config.from_object(config)
-    #ORM_mysql
+    # ORM_mysql
     db.init_app(app)
-    migrate.init_app(app,db)
-    #blueprint
+    migrate.init_app(app, db)
+    # blueprint
     from .views import main_views
     app.register_blueprint(main_views.bp)
-    
+
     return app
