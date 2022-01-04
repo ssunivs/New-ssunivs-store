a9eaa02f6d9240bb77932b2428ee584fa48bc2b3
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-01-05 00:42:23 +0900

    models import & requirements

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
+from . import models
 import config
 
 db = SQLAlchemy()
